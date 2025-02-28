144e45ce02ad65764b96b3ea678f64d461505a3e
Sanjay Kumar P <sanjuchary5895@gmail.com>
2025-02-28 12:25:19 +0530

    Slider_Animation

--- src/components/Cards/DesignCard/index.tsx
+++ src/components/Cards/DesignCard/index.tsx
@@ -5,23 +5,59 @@ import gsap from "gsap";
 
 const DesignCard = () => {
   const carouselRef = useRef(null);
+  const animatedLineRef = useRef(null); // Ref to the SVG path
 
   useEffect(() => {
-    const tl = gsap.timeline({ repeat: -1 });
+    // GSAP timeline for the carousel sliding animation
+    const tl = gsap.timeline({ repeat: -1, paused: true });
 
+    // Animation for moving the carousel slides
     tl.to(carouselRef.current, {
       x: "-100vw", // Move left to next slide
-     // Smooth slide duration
-     duration:1,
+      duration: 1,
       ease: "power2.inOut",
     })
-      .to(carouselRef.current, { x: "-100vw", duration:5 }) // Stay for 5s
+      .to(carouselRef.current, { x: "-100vw", duration: 5 }) // Stay for 5s
       .to(carouselRef.current, {
         x: "0vw", // Move back to the first slide
-        duration:1,
+        duration: 1,
         ease: "power2.inOut",
       })
       .to(carouselRef.current, { x: "0vw", duration: 5 }); // Stay for 5s
+
+    // Function to animate the SVG line
+    const animateLine = () => {
+      const path = animatedLineRef.current;
+      const length = path.getTotalLength();
+
+      // Reset the path before starting the animation
+      gsap.set(path, {
+        strokeDasharray: length,
+        strokeDashoffset: length,
+      });
+
+      // Animate the strokeDashoffset to 0 (fully revealed)
+      gsap.to(path, {
+        strokeDashoffset: 0,
+        duration: 10,
+        ease: "power2.inOut",
+      });
+    };
+
+    // Trigger animation on the first slide and on each slide transition
+    const handleSlideChange = () => {
+      animateLine();
+    };
+
+    // Play the timeline animation
+    tl.play();
+
+    // Call handleSlideChange whenever the timeline changes the slide
+    tl.eventCallback("onStart", handleSlideChange); // Trigger the animation when a slide starts
+    tl.eventCallback("onReverseComplete", handleSlideChange); // Reset on reverse to animate the next slide
+
+    // Start the animation loop
+    tl.play();
   }, []);
 
   return (
@@ -34,74 +70,154 @@ const DesignCard = () => {
         }}
       >
         {/* First Slide */}
-        <Box sx={{ minWidth: "100vw", display: "flex", justifyContent: "center" }}>
+        <Box
+          sx={{
+            minWidth: "100vw",
+            display: "flex",
+            justifyContent: "center",
+            position: "relative",
+          }}
+        >
           <Box
             sx={{
-              width: "40%",
+              position: "absolute",
+              top: 0,
+              width: "100%",
+              height: "100%",
+              zIndex: -1,
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1000 100"
+              style={{
+                width: "100%",
+                height: "100%",
+              }}
+            >
+              <path
+                ref={animatedLineRef}
+                d="M1 220C1 220 500.5 -100.5 353.5 100.5C206.5 301.5 647.5 161.5 647.5 161.5V133L761.5 19.0002C795.5 19.0002 792 31 792 49L678.314 161.5C678.314 161.5 1285 -106 1122 49C959 204 1437 161.5 1437 161.5"
+                stroke="lightgreen"
+                strokeWidth="5"
+                fill="none"
+              />
+            </svg>
+          </Box>
+          <Box
+            sx={{
+              width: "80%",
               display: "flex",
-              flexDirection: "column",
+              flexDirection: "row",
               justifyContent: "flex-start",
               alignItems: "flex-start",
               paddingTop: "50px",
               gap: 3,
             }}
           >
-            <Typography
-              sx={{
-                backgroundColor: "lightgreen",
-                width: "26%",
-                fontSize: "50px",
-                fontWeight: 600,
-                padding: "0 10px",
-              }}
-            >
-              Design
-            </Typography>
-            <Typography sx={{ width: "60%" }}>
-              We create an attractive UI/UX design after conducting thorough research
-              on your niche which helps to survive in this ever-evolving business
-              world.
-            </Typography>
-            <Button variant="outlined">KNOW MORE</Button>
-          </Box>
-          <Box>
-            <img height="420px" src={designimg} alt="" />
+            <Box>
+              <img
+                height="420px"
+                src={designimg}
+                style={{ transform: "scaleX(-1)" }}
+                alt=""
+              />
+            </Box>
+            <Box>
+              <Typography
+                sx={{
+                  backgroundColor: "lightgreen",
+                  width: "26%",
+                  fontSize: "50px",
+                  fontWeight: 600,
+                  padding: "0 10px",
+                }}
+              >
+                Design
+              </Typography>
+              <Typography sx={{ width: "60%" }}>
+                We create an attractive UI/UX design after conducting thorough
+                research on your niche which helps to survive in this
+                ever-evolving business world.
+              </Typography>
+              <Button variant="outlined">KNOW MORE</Button>
+            </Box>
           </Box>
         </Box>
 
         {/* Second Slide */}
-        <Box sx={{ minWidth: "100vw", display: "flex", justifyContent: "center" }}>
+        <Box
+          sx={{
+            minWidth: "100vw",
+            display: "flex",
+            justifyContent: "center",
+            position: "relative",
+          }}
+        >
           <Box
             sx={{
-              width: "40%",
+              position: "absolute",
+              top: 0,
+              width: "100%",
+              height: "100%",
+              zIndex: -1,
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1000 100"
+              style={{
+                width: "100%",
+                height: "100%",
+              }}
+            >
+              <path
+                ref={animatedLineRef}
+                d="M1 220C1 220 500.5 -100.5 353.5 100.5C206.5 301.5 647.5 161.5 647.5 161.5V133L761.5 19.0002C795.5 19.0002 792 31 792 49L678.314 161.5C678.314 161.5 1285 -106 1122 49C959 204 1437 161.5 1437 161.5"
+                stroke="lightgreen"
+                strokeWidth="5"
+                fill="none"
+              />
+            </svg>
+          </Box>
+          <Box
+            sx={{
+              width: "80%",
               display: "flex",
-              flexDirection: "column",
+              flexDirection: "row",
               justifyContent: "flex-start",
               alignItems: "flex-start",
               paddingTop: "50px",
               gap: 3,
             }}
           >
-            <Typography
-              sx={{
-                backgroundColor: "lightgreen",
-                width: "26%",
-                fontSize: "50px",
-                fontWeight: 600,
-                padding: "0 10px",
-              }}
-            >
-              Design
-            </Typography>
-            <Typography sx={{ width: "60%" }}>
-              We create an attractive UI/UX design after conducting thorough research
-              on your niche which helps to survive in this ever-evolving business
-              world.
-            </Typography>
-            <Button variant="outlined">KNOW MORE</Button>
-          </Box>
-          <Box>
-            <img height="420px" src={designimg} alt="" />
+            <Box>
+              <img
+                height="420px"
+                src={designimg}
+                style={{ transform: "scaleX(-1)" }}
+                alt=""
+              />
+            </Box>
+            <Box>
+              <Typography
+                sx={{
+                  backgroundColor: "lightgreen",
+                  width: "26%",
+                  fontSize: "50px",
+                  fontWeight: 600,
+                  padding: "0 10px",
+                }}
+              >
+                Develop
+              </Typography>
+              <Typography sx={{ width: "60%" }}>
+                We create an attractive UI/UX design after conducting thorough
+                research on your niche which helps to survive in this
+                ever-evolving business world.
+              </Typography>
+              <Button variant="outlined">KNOW MORE</Button>
+            </Box>
           </Box>
         </Box>
       </Box>
--- src/components/Center_Container/index.tsx
+++ src/components/Center_Container/index.tsx
@@ -1,49 +1,71 @@
 import { useGSAP } from "@gsap/react";
-import { Box } from "@mui/material"
+import { Box, Typography } from "@mui/material";
 import gsap from "gsap";
 
 export const Center_Container = () => {
-    useGSAP(() => {
+  useGSAP(() => {
     gsap.from(".animated-line", {
-        delay: 2,
-        duration: 5,
-        strokeDasharray: 500,
-        strokeDashoffset: 500,
-        ease: "power2.inOut",
-      });
+      delay: 2,
+      duration: 5,
+      strokeDasharray: 1000,
+      strokeDashoffset: 1000,
+      ease: "power2.inOut",
     });
+  });
   return (
-   <Box>
-      <svg
-        width="100%"
-        
-        height="400"
-        viewBox="0 0 1000 100"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        style={{border:"1px solid red" }}
+    <Box>
+      <Box
+        sx={{
+          position: "relative",
+        }}
       >
-        <path
-          className="animated-line"
-        //   d="M50,250 C10,10 10,20 3,100 L50,100 L50,100 L100,100 L100,50 L150,50 L150,100 L200,100 L200,250    C700,300 200,20 0,200  C100,200 20,0 700 C100,200 20,0 700,-20 C700,300 200,20 0,200 Z"
-//           d="M1000,300  
-// C950,250 920,150 880,200  
-// L880,20 L840,50 L840,200  
-// C800,100 750,350 700,200  
-// L700,200 L660,100 L660,200  
-// C600,200 550,150 500,300  
-// C450,150 400,100 350,150  
-// C300,100 250,300 200,250  
-// C150,50 100,100 50,300"
-          d="M1300,5 C900,300 -300,-80 500,-50 800,-50 500,300 200,100 20,1000"
-          stroke="#E73A3C"
-          strokeWidth="5"
+        <svg
+          width="100%"
+          height="1000"
+          viewBox="0 0 1000 100"
           fill="none"
-        />
-        {/* <circle cx="200" cy="50" r="5" fill="#E73A3C" />
+          xmlns="http://www.w3.org/2000/svg"
+          style={{ border: "1px solid red" }}
+        >
+          <path
+            className="animated-line"
+            // d="M50,250 C10,10 10,20 3,100 L50,100 L50,100 L100,100 L100,50 L150,50 L150,100 L200,100 L200,250    C700,300 200,20 0,200  C100,200 20,0 700 C100,200 20,0 700,-20 C700,300 200,20 0,200 Z"
+            //           d="M1000,300
+            // C950,250 920,150 880,200
+            // L880,20 L840,50 L840,200
+            // C800,100 750,350 700,200
+            // L700,200 L660,100 L660,200
+            // C600,200 550,150 500,300
+            // C450,150 400,100 350,150
+            // C300,100 250,300 200,250
+            // C150,50 100,100 50,300"
+            // d="M1000 200 600 200"
+            // C1000,150 920,150 880,200"
+            // d="M1000,2 L850,250"
+            // d="M150,250 C300,100 700,100 500,250 C300,400 700,400 850,250"
+            // d="M2 230C145.2 385.2 494 364 538 308C583 239 425 145.5 384 179C318.4 232.6 329 419 937 354"
+            //MAin
+            // d="M-1 1C221.707 327.107 692.978 268.264 761.407 150.597C831.392 5.61383 605.693 73.9675 510.469 89.2867C351.929 114.792 494.428 257.256 1440 120.677"
+            //Pencil
+            d="M1 220C1 220 500.5 -100.5 353.5 100.5C206.5 301.5 647.5 161.5 647.5 161.5V133L761.5 19.0002C795.5 19.0002 792 31 792 49L678.314 161.5C678.314 161.5 1285 -106 1122 49C959 204 1437 161.5 1437 161.5"
+            stroke="#E73A3C"
+            strokeWidth="5"
+            fill="none"
+          />
+          {/* <circle cx="200" cy="50" r="5" fill="#E73A3C" />
         <circle cx="300" cy="50" r="5" fill="#E73A3C" />
         <circle cx="100" cy="50" r="5" fill="#E73A3C" /> */}
-      </svg>
-   </Box>
-  )
-}
+        </svg>
+        <Typography
+          variant="h1"
+          sx={{
+            position: "absolute",
+            top: 0,
+          }}
+        >
+          Hello
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
