d653ee8d8f57973ae68c0d55588caa14fdd4772a
Mahendar-77 <mahendarlokini@gmail.com>
2025-04-09 10:37:03 +0530

    routes created

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,13 @@
-import { useRef } from "react";
-import AboutCard from "./components/Cards/AboutCard";
-import DesignCard from "./components/Cards/DesignCard";
-import Marquee from "./components/Cards/Marquee";
-import Navbar from "./components/Navbar";
-// import Services from "./components/Services";
-import Squad from "./components/Cards/Squad";
-// import OurTeam from "./components/Cards/OurTeam";
-import Footer from "./components/Footer";
-import OurServices from "./components/Cards/OurSevices";
-// import { ServiceCards } from "./components/Cards/ServicesCard";
-import ContactUs from "./components/ContactUs";
-// import OurTeam from "./components/Cards/OurTeam";
-// import { ServiceCards } from "./components/Cards/ServicesCard";
+import { Main } from "./Routes/Main";
 
-// import OurTeam from "./components/Cards/OurTeam";
-
-export type SectionRefs = {
-  aboutRef: React.RefObject<HTMLDivElement | null>;
-  servicesRef: React.RefObject<HTMLDivElement | null>;
-  contactRef: React.RefObject<HTMLDivElement | null>;
-  // design: React.RefObject<HTMLDivElement | null>;
-  // marquee: React.RefObject<HTMLDivElement | null>;
-  // center: React.RefObject<HTMLDivElement | null>;
-};
 
 function App() {
-  const sectionRefs: SectionRefs = {
-    aboutRef: useRef<HTMLDivElement | null>(null),
-    servicesRef: useRef<HTMLDivElement | null>(null),
-    contactRef: useRef<HTMLDivElement | null>(null),
-    // design: useRef<HTMLDivElement | null>(null),
-    // marquee: useRef<HTMLDivElement | null>(null),
-    // center: useRef<HTMLDivElement | null>(null),
-  };
   // const aboutRef = useRef(null);
 
   return (
     <>
-  
-      <Navbar sectionRefs={sectionRefs} />
-      <DesignCard />
-      {/* <Services ref={sectionRefs.servicesRef} /> */}
-      <OurServices ref={sectionRefs.servicesRef}/>
-      <AboutCard ref={sectionRefs.aboutRef} />
-      <Marquee />
-      {/* <OurTeam /> */}
-      <Squad />
-      <ContactUs ref={sectionRefs.contactRef} />
-      <Footer />
-      {/* <Center_Container ref={sectionRefs.center}/> */}
+      <Main/>
     </>
-    //   <>
-    //   <Navbar aboutRef={aboutRef} /> {/* Pass ref to Navbar */}
-    //   <DesignCard />
-    //   <Services />
-    //   <AboutCard ref={aboutRef} /> {/* Attach ref to AboutCard */}
-    //   <Marquee />
-    //   {/* <Center_Container /> */}
-    // </>
   );
 }
 export default App;
